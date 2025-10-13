e01bc542d1f81378929d5b7385e9c40d9b4bbf3e
AsadbekAlimov-Frondend-Developer <a37335877@gmail.com>
2025-10-13 17:00:30 +0500

    tugayapti 👌

--- news_project/urls.py
+++ news_project/urls.py
@@ -1,22 +1,9 @@
 from django.urls import path
-from .views import all_news
+from .views import all_news, detail, home_page_view
 
 
 urlpatterns = [
-    path('all_news/', all_news, name='all_news')
+    path('all_news/', all_news, name='all_news'),
+    path('all_news/<int:pk>/', detail, name='detail'),
+    path('', home_page_view, name='home_page'),
 ]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- news_project/views.py
+++ news_project/views.py
@@ -7,3 +7,20 @@ def all_news(request):
         'news':news
     }
     return render(request, 'news/all_news.html',context)
+
+def detail(request, pk):
+    new = News.objects.get(id=pk)
+    context = {
+        'new':new
+    }
+    return render(request, 'news/one.html', context)
+
+def home_page_view(request):
+    categories = Category.objects.all()
+    news = News.objects.all()
+    context = {
+        'categories': categories,
+        'news': news
+    }
+    return render(request, 'news/index.html', context)
+
